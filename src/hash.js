410f938f4c290c1d85ace9869f3d450d070f5860
antipachita <nice.strogonov@inbox.ru>
2022-06-12 14:39:00 +0300

    feat: hash fix

--- src/hash.js
+++ src/hash.js
@@ -4,20 +4,24 @@ import {createReadStream} from 'fs';
 
 export const getHash = (filePath) => {
     
-    // fs.readFile(filePath, (err, data) => {
-    //     const hash = crypto.createHash('sha256');
-    //     const hex = hash.update(data.toString()).digest('hex');
-      
-    //   console.log(hex);
-    // });
+  
     const readStream = createReadStream(filePath);
+    const fileVoid = [];
     readStream.on('error', (err) => {
         console.error('whoops! there was an error');
     });
-    readStream.on('data', (chunk) =>{
+      readStream.on('data', (chunk) =>{
       const hash = crypto.createHash('sha256');
       const hex = hash.update(chunk.toString()).digest('hex');
-    
       console.log(hex);
+      const result = chunk.toString();
+      fileVoid.push(chunk.toString());
+      
+      
   })
+    readStream.on('end', () => {if (fileVoid.length === 0) {
+      console.log('e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855');
+    }});
+
+      
 };
